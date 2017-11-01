48f8caaf8674f4e7e46611a9f618c9f9d7d1987f
hodayaCohen <hodaya0444@gmail.com>
2017-11-01 21:48:35 +0200

    first time

---
+++ L61/app.js
@@ -0,0 +1,14 @@
+//Define the 'hodaya' module
+
+var hodaya = angular.module('hodaya', []);
+
+hodaya.controller('Math',function Math($scope){
+    $scope.book = 'my name';
+});
+
+hodaya.controller('Bryce', function Bryce($scope){//השם של הפונקציה חייב להיות שווה לשם הראשון.
+    $scope.myFirstVar = 'my name2';
+
+hodaya
+
+});
---
+++ L61/bookName.js
@@ -0,0 +1,5 @@
+//Define the 'bookname' module.
+
+ var bookName  = angular.module('bookName', []);
+
+bookName.controller =()
