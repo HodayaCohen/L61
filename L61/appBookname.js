ebbd352264df2b234affceb4cad85be6b93ff73b
hodayaCohen <hodaya0444@gmail.com>
2017-11-21 19:57:12 +0200

    L66

--- L61/appBookname.js
+++ L61/appBookname.js
@@ -1,8 +1,21 @@
 //הויו לא יכול לתקשר מול ה סביס, אלא רק באמצעות הכונטרולר
 //שיעור 61, קפצנו לשיעור 64, תרגול קונטרולר וסרביס - הוספה לסל קניות
 //שיעור 65 למדנו על פילטרים, פילטרי בסיס וגם פילטר בבניה עצמית כמו cap.... יש קובץ פילטר מופרד וגם קישור בביו
+//שיעור 66 למדנו על ראוטר
 
-var bookName  = angular.module('bookName',[]);
+
+var bookName  = angular.module('bookName',['ngRoute']);
+
+//שיעור 66  19.11 
+bookName.config(function($routeProvider){
+    $routeProvider
+    .when("/",{
+        templateUrl : "books.html"
+    })
+    .when('/order',{
+        templateUrl : "order.html"
+    })
+});
 
 bookName.service('cardservie', function(){
     this.booksInCart = [];
@@ -25,13 +38,10 @@ bookName.service ('orderService', function(){
 
 bookName.controller('student', function Math($scope,cardservie){
     $scope.books=[
-        
      new book('history','images/images1.jpg',123),
      new book('nath','images/images1.jpg',234 ),
      new book('ttrigo','images/images3.png',345)
     ]
-
-
 
     function book(name, imgUri, isbn){
         this.name=name;
